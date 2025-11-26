2666d86282470cee600c2d262c85960842378417
ReynierXZN <20247080@s.ubaguio.edu>
2025-11-26 18:03:08 +0800

    sync

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { IncidentReportComponent } from './incident-report/incident-report';
-import { ReliefDistribution } from './relief-distribution/relief-distribution';
-import { EvacuationCenter } from './evacuation-center/evacuation-center';
+import { ReliefDistributionComponent } from './relief-distribution/relief-distribution';
+import { EvacuationCenterComponent } from './evacuation-center/evacuation-center';
 
 export const routes: Routes = [
 
  { path: '', redirectTo: 'IncidentReportComponent', pathMatch: 'full' },
  { path: 'IncidentReportComponent', component: IncidentReportComponent },
- { path: 'ReliefDistribution', component: ReliefDistribution },
- { path: 'EvacuationCenter', component: EvacuationCenter}
+ { path: 'ReliefDistributionComponent', component: ReliefDistributionComponent },
+ { path: 'EvacuationCenterComponent', component: EvacuationCenterComponent}
 
 ];
 
--- src/app/relief-distribution/relief-distribution.ts
+++ src/app/relief-distribution/relief-distribution.ts
@@ -1,12 +1,75 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-relief-distribution',
-  imports: [CommonModule],
-  templateUrl: './relief-distribution.html',
-  styleUrl: './relief-distribution.css',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './relief-distribution.html'
 })
-export class ReliefDistribution {
+export class ReliefDistributionComponent {
 
+  collectionName = 'relief_distribution';
+
+  form = {
+    date: '',
+    barangay: '',
+    numberOfFamilies: '',
+    reliefItems: '',
+    distributedBy: '',
+  };
+
+  records: any[] = [];
+  editId: string | null = null;
+
+  constructor(private firestore: Firestore) {
+    const colRef = collection(this.firestore, this.collectionName);
+    collectionData(colRef, { idField: 'id' }).subscribe(data => {
+      this.records = data;
+    });
+  }
+
+  async save() {
+    const colRef = collection(this.firestore, this.collectionName);
+
+    if (this.editId) {
+      const docRef = doc(this.firestore, `${this.collectionName}/${this.editId}`);
+      await updateDoc(docRef, this.form);
+    } else {
+      await addDoc(colRef, this.form);
+    }
+
+    this.reset();
+  }
+
+  edit(record: any) {
+    this.editId = record.id;
+    this.form = { ...record };
+  }
+
+  async remove(id: string) {
+    const docRef = doc(this.firestore, `${this.collectionName}/${id}`);
+    await deleteDoc(docRef);
+  }
+
+  reset() {
+    this.form = {
+      date: '',
+      barangay: '',
+      numberOfFamilies: '',
+      reliefItems: '',
+      distributedBy: '',
+    };
+    this.editId = null;
+  }
 }
